5a974cac5fff899ec80a9413ea3c75450b06c519
Anton Hlushkov <115362431+antonhlushkov@users.noreply.github.com>
2022-11-02 14:15:46 +0200

    Initial commit

---
+++ Assets/Scripts/attr.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attr : MonoBehaviour
+{
+       [Range(0f, 10f)]
+public float speed = 5f;
+
+    static void Initialization ()
+{
+
+}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
